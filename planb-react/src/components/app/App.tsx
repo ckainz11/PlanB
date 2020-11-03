1d74ae63d6a0316ce6c5ccc474baac8276c36f88
greenappleFF <florian.flatscher@gmail.com>
2020-11-03 18:25:13 +0100

    Logo go brrrrr

--- planb-react/src/components/app/App.tsx
+++ planb-react/src/components/app/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          PoggersButthole
+          Logo go brrrrrr
         </a>
       </header>
     </div>
