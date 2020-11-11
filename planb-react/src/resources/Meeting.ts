e317ed208ed995acf7795c36d2fc0752947dd8f4
K1ju02 <K1ju02@users.noreply.github.com>
2020-11-11 16:35:39 +0100

    specified Meeting.ts
    firebase back in dependencies

--- planb-react/src/resources/Meeting.ts
+++ planb-react/src/resources/Meeting.ts
@@ -1,7 +1,11 @@
-interface Meeting{
+import firebase from 'firebase';
 
-
+export interface Meeting {
+    name: String
+    date: firebase.firestore.Timestamp
+    start: firebase.firestore.Timestamp
+    end: firebase.firestore.Timestamp
+    description: String
+    location: String
+    proposer: String
 }
-
-
-export default Meeting;
