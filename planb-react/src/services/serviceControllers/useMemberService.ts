a79f36067c304b235fa7ee9f88313e3a6944e665
K1ju02 <K1ju02@users.noreply.github.com>
2020-11-12 14:33:36 +0100

    fixed naming error in useMemberService.ts

--- planb-react/src/services/serviceControllers/useMemberService.ts
+++ planb-react/src/services/serviceControllers/useMemberService.ts
@@ -2,7 +2,7 @@ import {useCallback, useContext} from "react";
 import {useDatabaseSpaceElements} from "..";
 import {Band, User} from "../../resources";
 
-export function useBandService(band: Band | undefined): {members: (Array<User> | undefined)} {
+export function useMemberService(band: Band | undefined): {members: (Array<User> | undefined)} {
     const members = useDatabaseSpaceElements<User>(band && `bandSpace/${band.name}/members`, 'users');
 
     return {
