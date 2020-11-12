badcb8dd68fedd0c116c0b08bbc5dbe1fa1fb50f
greenappleFF <florian.flatscher@gmail.com>
2020-11-12 14:44:17 +0100

    Add services to index.ts

--- planb-react/src/services/index.ts
+++ planb-react/src/services/index.ts
@@ -4,6 +4,10 @@ export * from "./serviceResources/DefaultFirebaseConfig";
 
 export * from "./serviceControllers/useBandService";
 export * from "./serviceControllers/useMeetingService";
+export * from "./serviceControllers/useMemberService";
+export * from "./serviceControllers/useSongService";
+export * from "./serviceControllers/useUserService";
+
 
 export * from "./dateBase/useDatabaseValue";
 export * from "./dateBase/useDatabaseSpaceElements";
