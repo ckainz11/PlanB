121b20d311be0937d697a2b3f3f02b6f3f3150e4
greenappleFF <florian.flatscher@gmail.com>
2020-11-14 00:24:38 +0100

    Add better elements watching

--- planb-react/src/services/dataBase/useDatabaseElements.ts
+++ planb-react/src/services/dataBase/useDatabaseElements.ts
@@ -1,20 +1,57 @@
 import {DataBaseElement} from "../../resources";
 import {useDatabase} from "./useDatabase";
-import {useEffect, useState} from "react";
+import {useEffect, useReducer, useState} from "react";
+import firebase from "firebase";
 
-export function useDatabaseElements<T extends DataBaseElement>(pathToElement : string | undefined): (T[] | undefined)[] {
-    const [databaseValue] = useDatabase(pathToElement);
-    const [elements, setElements] = useState<T[]>();
+enum arrayAction {
+    add,
+    change,
+    remove,
+    clear,
+    undefine,
+}
+
+function addArrayReducer<T extends DataBaseElement>(state: undefined | T[], action: { type: arrayAction.add | arrayAction.change | arrayAction.remove; payload: T} | {type: arrayAction.clear | arrayAction.undefine}): undefined | T[] {
+    switch (action.type) {
+        case arrayAction.add:
+            return state && action.payload && [...state, action.payload];
+        case arrayAction.change:
+            return action.payload && state && [...state.filter((e) => action.payload?.dataBaseID && e.dataBaseID !== action.payload.dataBaseID), action.payload];
+        case arrayAction.remove:
+            return action.payload && state && [...state.filter((e) => action.payload?.dataBaseID && e.dataBaseID !== action.payload.dataBaseID)];
+        case arrayAction.clear:
+            return [];
+        case arrayAction.undefine:
+            return undefined;
+        default:
+            throw new Error("Invalid action");
+    }
+}
+
+export function useDatabaseElements<T extends DataBaseElement>(pathToElements: string | undefined): (T[] | undefined)[] {
+    const [elements, dispatch] = useReducer(addArrayReducer, undefined);
 
     useEffect(() => {
-        if (databaseValue?.val()) {
-            setElements(Object.keys(databaseValue?.val()).map(key => {
-                return {dataBaseID: key, ...databaseValue?.val()[key]}
-            }));
-        } else {
-            setElements(undefined);
-        }
-    }, [databaseValue]);
+        dispatch({type: arrayAction.clear});
 
-    return [elements];
+        if (pathToElements) {
+            const ref = firebase.database().ref(pathToElements);
+            ref.on('child_added', function (childSnapshot) {
+                dispatch({type: arrayAction.add, payload:{dataBaseID: childSnapshot.key, ...childSnapshot.val()}})
+            });
+
+            ref.on('child_removed', function (oldChildSnapshot) {
+                dispatch({type: arrayAction.remove, payload:{dataBaseID: oldChildSnapshot.key, ...oldChildSnapshot.val()}})
+            });
+
+            ref.on('child_changed', function (childSnapshot) {
+                dispatch({type: arrayAction.remove, payload:{dataBaseID: childSnapshot.key, ...childSnapshot.val()}})
+            });
+        } else {
+            dispatch({type: arrayAction.undefine});
+        }
+
+    }, [pathToElements]);
+
+    return [elements && elements as T[]];
 }
