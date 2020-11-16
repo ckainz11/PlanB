519b9befae13d13bf9263533a74a99fcdc8aa6be
greenappleFF <florian.flatscher@gmail.com>
2020-11-16 14:36:48 +0100

    Fix votes

--- planb-react/src/resources/Vote.ts
+++ planb-react/src/resources/Vote.ts
@@ -1,5 +1,7 @@
 import {DataBaseElement} from "./DataBaseElement";
+import {User} from "./User";
 
 export interface Vote extends DataBaseElement {
+    user: User,
     value: number;
 }
--- planb-react/src/services/serviceControllers/useVoteService.ts
+++ planb-react/src/services/serviceControllers/useVoteService.ts
@@ -1,11 +1,23 @@
-import {useDatabase, useDatabaseElements} from "..";
-import {Band, Meeting, Song, User, Vote} from "../../resources";
+import {useDatabase, useDatabaseElements, useMemberService, useUserService} from "..";
+import {Band, DataBaseElement, Meeting, Song, User, Vote} from "../../resources";
+import {type} from "os";
 import {useEffect, useState} from "react";
 
-export function useVoteService(band:Band | undefined, meeting:Meeting | undefined): (Vote[] | undefined)[] {
-    const [votes] = useDatabaseElements<Vote>(band && meeting && `bandSpace/${band.dataBaseID}/meetingSpace/${meeting.dataBaseID}/votes`);
+interface databaseVote extends DataBaseElement {
+    value: number
+}
 
+export function useVoteService(band:Band | undefined, meeting:Meeting | undefined): (Vote[] | undefined)[] {
+    const [databaseVotes] = useDatabaseElements<databaseVote>(band && meeting && `bandSpace/${band.dataBaseID}/meetingSpace/${meeting.dataBaseID}/votes`);
+    const [users] = useMemberService(band);
+    const [votes, setVotes] = useState<Vote[]>();
+
+    useEffect(() => {
+        databaseVotes && users && users?.map((user) => {
+            return {dataBaseID: user.dataBaseID, value: databaseVotes.find((vote) => vote.dataBaseID === user.dataBaseID) || 0} as Vote
+        });
+    }, [databaseVotes, users])
     return [
-        votes
+
     ];
 }
