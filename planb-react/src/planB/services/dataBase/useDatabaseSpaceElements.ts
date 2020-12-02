c7abb36228985a17729ffd7d64e2735f76ea8670
greenappleFF <florian.flatscher@gmail.com>
2020-12-02 22:27:52 +0100

    Finish off band service :)

--- planb-react/src/planB/services/dataBase/useDatabaseSpaceElements.ts
+++ planb-react/src/planB/services/dataBase/useDatabaseSpaceElements.ts
@@ -6,7 +6,9 @@ import {type} from "os";
 
 export function useDatabaseSpaceElements<T extends DataBaseElement>(pathToSpace: string | undefined, pathToElements: string | undefined): (T[] | undefined)[] {
     const [elements, dispatch] = useReducer(ArrayReducer, undefined);
-    const listenersRef = useRef(new Map<string, any>());
+
+    //Be careful! This data structure was a vary bad idea... :(
+    const listenersRef = useRef(new Map<string, [firebase.database.Reference, () => any]>());
 
     //If any path changes then reload all listeners
     useEffect(() => {
@@ -25,9 +27,10 @@ export function useDatabaseSpaceElements<T extends DataBaseElement>(pathToSpace:
                 if (!snapshot.val()) {
                     dispatch({type: ArrayAction.clear});
                 }
-            }, (err: any) => {if (err) console.error(err)});
+            }, (err: any) => {
+                if (err) console.error(err)
+            });
 
-            console.log("Path changesd")
 
             //Listen to ids of added element
             const childAdd = ref.on('child_added', function (childSnapshot) {
@@ -36,33 +39,44 @@ export function useDatabaseSpaceElements<T extends DataBaseElement>(pathToSpace:
                     const elementRef = firebase.database().ref(pathToElements + "/" + childSnapshot.key)
 
                     //Add a listener to listen to its value and store it for later cleaning
-                    listeners.set(childSnapshot.key, elementRef.on("value", snapshot => {
+                     const elementListener = elementRef.on("value", snapshot => {
+                        console.log("Band changed: "+snapshot.key)
                         //Update or add the element
                         snapshot.key && dispatch({
                             type: ArrayAction.change,
                             payload: {dataBaseID: snapshot.key, ...snapshot.val()}
                         })
-                    }, (err: any) => {if (err) console.error(err)}));
+                    }, (err: any) => {
+                        if (err) console.error(err)
+                    });
+
+                    listeners.set(childSnapshot.key, [elementRef, elementListener]);
                 }
-            }, (err: any) => {if (err) console.error(err)});
+            }, (err: any) => {
+                if (err) console.error(err)
+            });
 
             const childRemove = ref.on('child_removed', function (oldChildSnapshot) {
+                console.log("Band removed from list: " + oldChildSnapshot.key)
                 if (oldChildSnapshot.key) {
+                    const listenerArr = listeners.get(oldChildSnapshot.key);
+                    listenerArr[0].off("value", listenerArr[1]);
                     listeners.delete(oldChildSnapshot.key);
                     dispatch({
                         type: ArrayAction.remove,
                         payload: {dataBaseID: oldChildSnapshot.key}
                     });
                 }
-            }, (err: any) => {if (err) console.error(err)});
+            }, (err: any) => {
+                if (err) console.error(err)
+            });
 
             return () => {
                 ref.off("child_added", childAdd);
                 ref.off("child_removed", childRemove);
 
                 listeners.forEach((value) => {
-                    ref.off("value", listeners.get(value));
-
+                    value[0].off("value", listeners.get(value[1]));
                 });
 
                 listeners.clear();
