5342a7deca223dbd0715aabea1cf3e865f2fca5c
greenappleFF <florian.flatscher@gmail.com>
2020-12-02 20:23:12 +0100

    Fix vote service

--- planb-react/src/planB/services/dataBase/useDatabaseSingleElement.ts
+++ planb-react/src/planB/services/dataBase/useDatabaseSingleElement.ts
@@ -7,6 +7,7 @@ export function useDatabaseSingleElement<T extends DataBaseElement>(pathToElemen
     const [element, setElement] = useState<T>();
 
     useEffect(() => {
+
         if (databaseValue) {
             setElement({dataBaseID: databaseValue.key, ...databaseValue.val()})
         } else {
