fdd21dafcf02fe78cd57bfddead26bb941ce4f7a
Christoph Kainz <ckainz11@users.noreply.github.com>
2020-12-27 15:55:43 +0100

    Fix Login css

--- planb-react/src/planB/components/LoginHeader.tsx
+++ planb-react/src/planB/components/LoginHeader.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import logo from "../../images/LogoPlanB.png";
 
 export const LoginHeader = () => {
-    return <div className="header">
-        <div className={"headerItem left"}>
+    return <div className="login-header">
+        <div className={"login-header left"}>
             <img className={"logo"} src={logo} alt={"logo"}/>
         </div>
         </div>
