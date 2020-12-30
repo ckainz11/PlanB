6ad443991103da676cf9d32c784df97197936c82
florian <florian.flatscher@gmail.com>
2020-12-30 13:58:02 +0100

    Update Bandservice

--- planb-react/src/planB/services/serviceControllers/useBandService.ts
+++ planb-react/src/planB/services/serviceControllers/useBandService.ts
@@ -1,26 +1,15 @@
 import firebase from "firebase";
-import {useCallback, useEffect} from "react";
-import {useDatabaseSpaceElements} from "../index";
-import {Band, User} from "../../resources";
+import { useCallback, useEffect } from "react";
+import { useDatabaseSpaceElements } from "../index";
+import { Band, User } from "../../resources";
 
 type OperationType =
     { type: "add", payload: Band } |
     { type: "remove", payload: Band }
     ;
 
-export function useBandService(user: User | undefined): [Band[] | undefined, (operation: OperationType) => void] {
+export function useBandService(user: User | undefined): [Band[] | undefined, (operation: OperationType) => Promise<void>] {
     let [bands] = useDatabaseSpaceElements<Band>(user && `userSpace/${user.dataBaseID}/bands`, 'bands');
-
-    // useEffect(() => {
-    //     if (bands && user) {
-    //         bands = bands.filter((band) => {
-    //             if (!band.dataBaseID) {
-    //                 return false
-    //             }
-    //             return true
-    //         })
-    //     }
-    // }, [bands])
 
     const bandOperation = useCallback(async (operation: OperationType) => {
         if (user) {
@@ -36,40 +25,40 @@ export function useBandService(user: User | undefined): [Band[] | undefined, (op
                         return;
                     }
 
-                    firebase.database().ref("bandSpace/" + bandID + "/members/" + user.dataBaseID).set(true)
-                        .catch(error => {
-                            error && console.log("2.1 " + error)
-                        });
-
-                    firebase.database().ref("userSpace/" + user.dataBaseID + "/bands/" + bandID).set(true)
-                        .catch(error => {
-                            error && console.log("3 " + error)
-                        });
-
-                    break;
-                case "remove":
-                    let members: string[] = [];
-                    await firebase.database().ref("bandSpace/" + operation.payload.dataBaseID + "/members").once("value", (snapshot) => {
-                        if (!snapshot.val()) {
-                            return
-                        }
-                        members = (Object.keys(snapshot.val()).map((key) => {
-                            return key
-                        }))
-                    })
-
-                    if (members.length === 0) {
-                        members.push(user.dataBaseID)
+                    try {
+                        await firebase.database().ref("bandSpace/" + bandID + "/members/" + user.dataBaseID).set(true)
+                        await firebase.database().ref("userSpace/" + user.dataBaseID + "/bands/" + bandID).set(true)
+                    } catch (err) {
+                        console.error(err)
                     }
+                    break;
 
-                    firebase.database().ref("bandSpace/" + operation.payload.dataBaseID).remove()
-                        .then(() => {
-                            firebase.database().ref("bands/" + operation.payload.dataBaseID).remove()
-                                .catch(error => console.log(error));
-                            members.forEach((member) => {
-                                firebase.database().ref("userSpace/"+member+"/bands/" + operation.payload.dataBaseID).remove().catch(err => console.log(err))
-                            })
-                        }).catch(error => console.log(error));
+                case "remove":
+                    try {
+
+                        let members: string[] = [];
+                        await firebase.database().ref("bandSpace/" + operation.payload.dataBaseID + "/members").once("value", (snapshot) => {
+                            if (!snapshot.val()) {
+                                return
+                            }
+                            members = (Object.keys(snapshot.val()).map((key) => {
+                                return key
+                            }))
+                        })
+
+                        if (members.length === 0) {
+                            members.push(user.dataBaseID)
+                        }
+
+                        await firebase.database().ref("bandSpace/" + operation.payload.dataBaseID).remove()
+                        await firebase.database().ref("bands/" + operation.payload.dataBaseID).remove()
+                        members.forEach(async (member) => {
+                            await firebase.database().ref("userSpace/" + member + "/bands/" + operation.payload.dataBaseID).remove();
+                        })
+                        break;
+                    } catch (err) {
+                        console.error(err)
+                    }
                     break;
             }
         }
