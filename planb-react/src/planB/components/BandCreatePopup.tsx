ba19422c9cfc427b6f34e8a95c4f1ee47fdbdd34
florian <florian.flatscher@gmail.com>
2021-01-04 22:33:54 +0100

    Best practises

--- planb-react/src/planB/components/BandCreatePopup.tsx
+++ planb-react/src/planB/components/BandCreatePopup.tsx
@@ -4,6 +4,17 @@ import {Band, Song, User} from "../resources";
 import {useBandService, useMemberService, usePersonalService, useUserService} from "../services";
 import {BandContext} from "../contexts";
 
+function getOptions (users: User[], me: User) {
+    const options = users?.filter(user => user.dataBaseID !== me?.dataBaseID).map(user => {
+        return {
+            key: user.dataBaseID,
+            text: user.userName,
+            value: user.dataBaseID,
+            image: {avatar: true, src: user.photoUrl}
+        }
+    })
+    return options;
+}
 
 export const BandCreatePopup = ({open, onClose, selectBand, me}: BandCreatePopupProps) => {
     const [newBand, setNewBand] = useState<Band>({leader: me ? me.dataBaseID : ""} as Band)
@@ -23,20 +34,6 @@ export const BandCreatePopup = ({open, onClose, selectBand, me}: BandCreatePopup
         selectBand(newBand)
     }, [bandOperation, memberList, selectBand, newBand, onClose]);
 
-
-
-
-    const options = users?.filter(user => user.dataBaseID !== me?.dataBaseID).map(user => {
-        return {
-            key: user.dataBaseID,
-            text: user.userName,
-            value: user.dataBaseID,
-            image: {avatar: true, src: user.photoUrl}
-        }
-    })
-
-
-
     return <Modal open={open} onClose={() => onClose()} closeIcon>
         <Modal.Header>Create a new Band</Modal.Header>
         <Modal.Content>
@@ -51,7 +48,7 @@ export const BandCreatePopup = ({open, onClose, selectBand, me}: BandCreatePopup
                 </FormField>
                 <FormField>
                     <Dropdown placeholder={"Members"} selection multiple fluid search pointing={"bottom"}
-                              options={options} onChange={((event, data) => {
+                              options={getOptions(users || [], me)} onChange={((event, data) => {
                         const newMemberList: string[] = []
                         const m = data.value as []
                         m.forEach(userid => {
