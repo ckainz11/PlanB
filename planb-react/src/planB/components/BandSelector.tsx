8cba42e1014073eee3ce21680c54806374626d42
Christoph Kainz <ckainz11@users.noreply.github.com>
2021-01-08 17:30:43 +0100

    Minor Css changes

--- planb-react/src/planB/components/BandSelector.tsx
+++ planb-react/src/planB/components/BandSelector.tsx
@@ -12,12 +12,14 @@ export const BandSelector = ({bands, selectBand, me}: BandSelectorProps) => {
 
 
     return <div>
+        <h2>Welcome back, {me.userName}!</h2>
+        <h3>Select a band to get started</h3>
         <Button icon="plus" content="Create new Band" className="color-positive" onClick={() => setOpen(true)} />
         {bands.map(band => {
             return <Container className="band-display" key={band.dataBaseID} onClick={() => selectBand(band)}>
                 <h2>{band.name}</h2>
-                <h4>Leader: {users?.find(user => user.dataBaseID === band.leader)?.userName}</h4>
-                <pre>{band.description}</pre>
+                <h3>Leader: {users?.find(user => user.dataBaseID === band.leader)?.userName}</h3>
+                <br/>
             </Container>
         })}
         {open && <BandCreatePopup open={open} onClose={close} selectBand={selectBand} me={me}/>}
