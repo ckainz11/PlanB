7c767be2a7aab02966fd679cc91cd1a96751b107
Kieran <kieran.kirchweger@gmail.com>
2021-01-11 12:56:50 +0100

    changed sessionValidation to use CustomError

---
+++ planb-react/src/planB/resources/CustomError.ts
@@ -0,0 +1,5 @@
+
+export type CustomError = {
+    field: string,
+    message: string
+}
--- planb-react/src/planB/services/serviceControllers/useSessionService.ts
+++ planb-react/src/planB/services/serviceControllers/useSessionService.ts
@@ -1,5 +1,5 @@
 import {useDatabaseElements} from "..";
-import {Band, Session, Song} from "../../resources";
+import {Band, Session, Song, CustomError} from "../../resources";
 import {useCallback, useEffect, useState} from "react";
 import firebase from "firebase/app";
 
@@ -9,7 +9,7 @@ type OperationType =
     { type: "addWithSongs", payload: { session: Session, songs: Song[] } }
     ;
 
-export function useSessionService(band: Band | undefined): [Session[], ((operation: OperationType) => Promise<void>), ((session: Session) => number)] {
+export function useSessionService(band: Band | undefined): [Session[], ((operation: OperationType) => Promise<void>), ((session: Session) => CustomError[])] {
     const [rawSessions] = useDatabaseElements<Session>(band?.dataBaseID && `bandSpace/${band.dataBaseID}/sessions`);
     const [compiledSessions, setCompiledSessions] = useState<Session[]>(rawSessions || []);
 
@@ -58,40 +58,65 @@ export function useSessionService(band: Band | undefined): [Session[], ((operati
     }, [band]);
 
     const sessionValidation = useCallback((session: Session) => {
+        let error = []
+
         if (!session.start || !session.end || session.start > session.end) {
-            return -1
+            error.push({
+                field: "start",
+                message: "Start must not be after end."
+                })
         }
         if (!session.name || session.name.length < 3) {
-            return -2
+            error.push({
+                field: "name",
+                message: "Name is too short."
+            })
         }
-        //if(session.name.split("")[0] === " ") {return -3}
         if (!session.name || session.name.length > 50) {
-            return -4
+            error.push({
+                field: "name",
+                message: "Name is too long."
+            })
         }
-        if (!session.start || !isNaN(session.start.getTime())) {
-            return -5
+        if (!session.start || isNaN(session.start.getTime())) {
+            error.push({
+                field: "start",
+                message: "Start is not a valid date."
+            })
         }
-        if (!session.end || !isNaN(session.end.getTime())) {
-            return -6
+        if (!session.end || isNaN(session.end.getTime())) {
+            error.push({
+                field: "end",
+                message: "End is not a valid date."
+            })
         }
         if (!session.start || session.start < new Date()) {
-            return -7
+            error.push({
+                field: "start",
+                message: "Start must not be in the past."
+            })
         }
         if (!session.location || session.location.length > 100) {
-            return -8
+            error.push({
+                field: "location",
+                message: "Location is too long."
+            })
         }
         if (!session.description || session.description.length > 2000) {
-            return -9
+            error.push({
+                field: "description",
+                message: "Description is too long."
+            })
         }
 
-        return 1;
+        return error;
     }, []);
 
     const sessionOperation = useCallback(async (operation: OperationType) => {
             if (band) {
                 switch (operation.type) {
                     case "add":
-                        if (await sessionValidation(operation.payload) < 0) {
+                        if (sessionValidation(operation.payload).length > 0) {
                             console.log("%c Validation failed. SessionService: 'add'", 'color: #D100D0')
                             return;
                         }
@@ -105,7 +130,7 @@ export function useSessionService(band: Band | undefined): [Session[], ((operati
                         break;
 
                     case "addWithSongs":
-                        if (await sessionValidation(operation.payload.session) < 0) {
+                        if (sessionValidation(operation.payload.session).length > 0) {
                             console.log("%c Validation failed. SessionService: 'addWithSongs'", 'color: #D100D0')
                             return;
                         }
