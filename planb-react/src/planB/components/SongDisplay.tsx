fc61def2ad7863f507fb6607dee00f0c6be8e17f
greenappleFF <florian.flatscher@gmail.com>
2021-02-14 11:28:14 +0100

    Fix css for songs

--- planb-react/src/planB/components/SongDisplay.tsx
+++ planb-react/src/planB/components/SongDisplay.tsx
@@ -5,7 +5,7 @@ import {useSongService} from "../services";
 
 const getCSS = (rating: number) => {
     let css = "song-score";
-    if(rating <= 5)
+    if (rating <= 5)
         css += " color-negative";
     else
         css += " color-positive";
@@ -19,18 +19,32 @@ export const SongDisplay = ({song, band}: SongDisplayProps) => {
 
     return <Container className={"song-display"}>
         <div className={"song-display-header"}>
-            <p className={getCSS(song.rating)} >{song.rating}</p>
-            <h3 className={"song-title"}>{song.name}</h3>
-            <Button circular  icon={details ? "arrow up" : "arrow down"} basic inverted onClick={() => setDetails(!details)} />
+            <p style={{flexShrink: 0}} className={getCSS(song.rating)}>{song.rating}</p>
+            <h3 style={{fontSize: "20px", marginLeft: "7px", marginRight: "7px"}} className={"song-title"}>{song.name}</h3>
+            <Button circular icon={details ? "arrow up" : "arrow down"} basic inverted
+                    onClick={() => setDetails(!details)}/>
         </div>
-      {details && <div className={"song-details"} >
-          <div className={"song-display-score-vote"}>
-              <Button size={"mini"} disabled={song.rating === 10} className={"color-positive song-btn"} icon="plus" onClick={() => songOperation({type: "update", payload: {...song, rating: song.rating + 1}})} />
-              <Button size={"mini"} disabled={song.rating === 0} className={"color-negative song-btn"} icon="minus" onClick={() => songOperation({type: "update", payload: {...song, rating: song.rating - 1}})} />
-          </div>
-            <p>{song.content}</p>
-          <Button size={"mini"} className={"color-negative song-btn"} icon="trash" onClick={() => songOperation({type: "remove", payload: song})} />
-        </div>}
+        {details && <>
+            <p style={{margin:"3px"}}>{song.content}</p>
+
+            <div className={"song-details"}>
+                <div className={"song-display-score-vote"}>
+                    <Button size={"mini"} disabled={song.rating === 10} className={"color-positive song-btn"}
+                            icon="plus" onClick={() => songOperation({
+                        type: "update",
+                        payload: {...song, rating: song.rating + 1}
+                    })}/>
+                    <Button size={"mini"} disabled={song.rating === 0} className={"color-negative song-btn"}
+                            icon="minus" onClick={() => songOperation({
+                        type: "update",
+                        payload: {...song, rating: song.rating - 1}
+                    })}/>
+                </div>
+                <Button size={"mini"} className={"color-negative song-btn"} icon="trash"
+                        onClick={() => songOperation({type: "remove", payload: song})}/>
+            </div>
+        </>
+        }
 
 
     </Container>
